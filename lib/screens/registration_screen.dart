a2bc53a9860cdc2220dabfeae5ba6e0523d5cfca
Adwaith Jayasankar <kichu31084@gmail.com>
2023-07-21 23:38:46 +0530

    first commit

---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flashchat/components/rounded_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flashchat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flashchat/screens/chat_screen.dart';
+import 'package:loader_overlay/loader_overlay.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  static const String id = 'registrationScreen';
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+
+  late String email;
+  late String password;
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: LoaderOverlay(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email.'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password'),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                colour: Colors.blueAccent,
+                buttonTitle: 'Register',
+                onPress: () async {
+                  setState(() {
+                    context.loaderOverlay.show();
+                  });
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                    setState(() {
+                      context.loaderOverlay.hide();
+                    });
+                  }
+                  catch(e){
+                    print(e);
+                  }
+
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
